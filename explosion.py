834d8b17a4c47926f1f57ca8c796c37c581148a4
dad3zero <dad3zero@gmail.com>
2022-07-25 19:43:49 +0200

    Simplified update using a comprehension list.

--- explosion.py
+++ explosion.py
@@ -5,22 +5,20 @@ import math
 WIDTH = 800
 HEIGHT = 600
 
-# how much a particle slows down by each second
-DRAG = 0.8 
 
-# the colour of each particle in R, G, B values
-PARTICLE_COLOR = 255, 230, 128
+DRAG = 0.8  # how much a particle slows down by each second
 
-# the time in seconds for which a particle is displayed
-MAX_AGE = 3
+PARTICLE_COLOR = 255, 230, 128  # the colour of each particle in R, G, B values
 
-# an array to hold the details of the explosion particles on the screen
-particles = []
+MAX_AGE = 3  # the time in seconds for which a particle is displayed
 
+particles = []  # an array to hold the details of the explosion particles on the screen
 
-# This function creates a new explosion at the specified screen co-ordinates
 
 def explode(x, y, speed=300):
+    """
+    This function creates a new explosion at the specified screen co-ordinates
+    """
 
     # these are new particles, so set their age to zero
     age = 0     
@@ -40,9 +38,10 @@ def explode(x, y, speed=300):
         particles.append((x, y, vx, vy, age))
 
 
-# This function redraws the screen by plotting each particle in the array
-
 def draw():
+    """
+    This function redraws the screen by plotting each particle in the array
+    """
 
     # clear the screen
     screen.clear()
@@ -54,9 +53,11 @@ def draw():
         screen.surface.set_at((int(x), int(y)), PARTICLE_COLOR)
 
 
-# This function updates the array of particles
-
 def update_particle_data(x, y, vx, vy, age, delay):
+    """
+    This function updates the array of particles
+    """
+
     drag = DRAG ** delay
     vx *= drag
     vy *= drag
@@ -69,26 +70,14 @@ def update_particle_data(x, y, vx, vy, age, delay):
     return x, y, vx, vy, age
 
 def update(dt):
-
-    # to update the particle array, create a new empty array
-    new_particles = []
-    
-    # loop through the existing particle array
-    for (x, y, vx, vy, age) in particles:
-    
-        # if a particle was created more than a certain time ago, it can be removed
-        if age + dt > MAX_AGE:
-            continue
-            
-        new_particles.append(update_particle_data(x, y, vx, vy, age, dt))
-        
-    # replace the current array with the new one
-    particles[:] = new_particles
-
-
-# This function creates an explosion at a random location on the screen
+    particles[:] = [update_particle_data(x, y, vx, vy, age, dt)
+                    for x, y, vx, vy, age in particles
+                    if age + dt <= MAX_AGE]
 
 def explode_random():
+    """
+    This function creates an explosion at a random location on the screen
+    """
 
     # select a random position on the screen
     x = random.randrange(WIDTH)
